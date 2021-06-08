99163c656a116c01f534fa913e02b9439d410a70
artiomorlovalex <artiomorlovalex@gmail.com>
2021-06-08 14:56:52 +0300

    Add more operators
    f

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-#Калькулятор на Python, версия 2.1
+#Калькулятор на Python, версия 2.2
 print("Привет! Это калькулятор! ")
 number1 = (input("Введи левый операнд "))
 try: float(number1)
@@ -6,15 +6,24 @@ except: print("Ошибка! Это не число!!!"), exit(0)
 number2 = (input("Введи правый операнд "))
 try: float(number2)
 except: print("Ошибка! Это не число!!!"), exit(0)
-operator = input("Ведите операцию (+, -, *, /, **): ") 
-if operator == "+" or operator == "-" or operator == "*" or operator == "/" or operator == "**":
+operator = input("Ведите операцию (+, -, *, /, //, %, **, &, |, ^, <<, >>): ") 
+if operator == "+" or operator == "-" or operator == "*" or operator == "/" or operator == "**" or operator == "//" or operator == "%" or operator == "&" or operator == "|" or operator == "^" or operator == "<<" or operator == ">>":
     if (operator == "+"): result = number1 + number2
     elif (operator == "-"): result = number1 - number2
     elif (operator == "*"): result = number1 * number2
     elif (operator == "**"): result = number1 ** number2
+    elif (operator == "%"): result = number1 % number2
+    elif (operator == "&"): result = number1 & number2
+    elif (operator == "|"): result = number1 | number2
+    elif (operator == "^"): result = number1 ^ number2
+    elif (operator == "<<"): result = number1 << number2
+    elif (operator == ">>"): result = number1 >> number2
     elif (operator == "/"):
         if (number2 == 0): print("На ноль делить нельзя!")
-        else: result = number1 / number2
+        else: result = number1 / number2, exit(0)
+    elif (operator == "//"):
+        if (number2 == 0): print("На ноль делить нельзя!")
+        else: result = number1 // number2 , exit(0)
 else: print("Вы неправильно ввели операцию или не ввели её!"), exit(0)
 print("Итог: ", result)
 
