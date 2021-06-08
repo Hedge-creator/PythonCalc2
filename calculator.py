c7a85bd0223a95cff7d93e8e3a7cf5e4189a6366
artiomorlovalex <artiomorlovalex@gmail.com>
2021-06-08 10:15:33 +0300

    calculator fixed4

--- calculator.py
+++ calculator.py
@@ -1,6 +1,11 @@
+#Калькулятор на Python, версия 2.1
 print("Привет! Это калькулятор! ")
-number1 = float(input("Введи левый операнд "))
-number2 = float(input("Введи правый операнд "))
+number1 = (input("Введи левый операнд "))
+try: float(number1)
+except: print("Ошибка! Это не число!!!"), exit(0)
+number2 = (input("Введи правый операнд "))
+try: float(number2)
+except: print("Ошибка! Это не число!!!"), exit(0)
 operator = input("Ведите операцию (+, -, *, /, **): ") 
 if operator == "+" or operator == "-" or operator == "*" or operator == "/" or operator == "**":
     if (operator == "+"): result = number1 + number2
@@ -10,6 +15,6 @@ if operator == "+" or operator == "-" or operator == "*" or operator == "/" or o
     elif (operator == "/"):
         if (number2 == 0): print("На ноль делить нельзя!")
         else: result = number1 / number2
-else:
-  print("Вы неправильно ввели операцию или ничего не ввели! Попробуйте ещё раз.")
+else: print("Вы неправильно ввели операцию или не ввели её!"), exit(0)
 print("Итог: ", result)
+
